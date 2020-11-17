5ba4b2036444fa7a4cc293349169d314fb15d8a5
rtnkanth <rtnkanth@gmail.com>
2020-11-17 20:05:21 +0530

    JPA Annotations(many2many)

--- src/main/java/rtnk/springframework/domain/Recipe.java
+++ src/main/java/rtnk/springframework/domain/Recipe.java
@@ -29,8 +29,8 @@ public class Recipe{
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
-        joinColumns = @JoinColumn(name="recipe_id"),
-             inverseJoinColumns = @JoinColumn(name="category_id"))
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories;
 
     public Long getId() {
