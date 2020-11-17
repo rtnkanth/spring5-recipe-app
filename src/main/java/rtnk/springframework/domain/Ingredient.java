16c105e9cdf1212920f749efc24b8a8f501214d4
rtnkanth <rtnkanth@gmail.com>
2020-11-17 15:18:51 +0530

    Added JPA Annotations

--- src/main/java/rtnk/springframework/domain/Ingredient.java
+++ src/main/java/rtnk/springframework/domain/Ingredient.java
@@ -11,6 +11,10 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -44,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
