7a138600488de609363e35f1c2ac62a7f609c7c5
rtnkanth <rtnkanth@gmail.com>
2020-11-22 10:00:52 +0530

    Lombok Refactoring

--- src/main/java/rtnk/springframework/domain/Category.java
+++ src/main/java/rtnk/springframework/domain/Category.java
@@ -19,27 +19,4 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
--- src/main/java/rtnk/springframework/services/RecipeServiceImpl.java
+++ src/main/java/rtnk/springframework/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package rtnk.springframework.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import rtnk.springframework.domain.Recipe;
 import rtnk.springframework.repositories.RecipeRepository;
@@ -7,6 +8,7 @@ import rtnk.springframework.repositories.RecipeRepository;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I m in Service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
