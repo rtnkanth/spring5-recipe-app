7993e8e290b78de0adf4d978ed6a82608b0660c4
rtnkanth <rtnkanth@gmail.com>
2020-11-23 17:08:58 +0530

    Implemented Bootstrap Css

--- src/main/java/rtnk/springframework/domain/Notes.java
+++ src/main/java/rtnk/springframework/domain/Notes.java
@@ -1,10 +1,12 @@
 package rtnk.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
