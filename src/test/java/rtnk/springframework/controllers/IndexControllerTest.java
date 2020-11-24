1b3dafaa91ed33f990dd5efd54b75991fcd75167
rtnkanth <rtnkanth@gmail.com>
2020-11-24 17:30:23 +0530

    Implemented IndexController Test

---
+++ src/test/java/rtnk/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,36 @@
+package rtnk.springframework.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import rtnk.springframework.services.RecipeService;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @Before
+    public void setUp()throws Exception{
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+    @Test
+    public void getIndexPage()throws Exception {
+        String viewName = controller.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+    }
+}
