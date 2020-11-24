2791095c61fdedd96a4b20eda7de00ec025c5188
rtnkanth <rtnkanth@gmail.com>
2020-11-24 22:56:14 +0530

    Implementing Spring Integration Test

---
+++ src/test/java/rtnk/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package rtnk.springframework.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import rtnk.springframework.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void findByDescription()throws Exception{
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+    @Test
+    public void findByDescriptionCup()throws Exception{
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
