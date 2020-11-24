1de7eb63885efd342b30f2311f9ca12302022715
rtnkanth <rtnkanth@gmail.com>
2020-11-24 19:18:38 +0530

    Implementing MockMvc

--- src/test/java/rtnk/springframework/controllers/IndexControllerTest.java
+++ src/test/java/rtnk/springframework/controllers/IndexControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import rtnk.springframework.domain.Recipe;
 import rtnk.springframework.services.RecipeService;
@@ -14,6 +16,9 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 public class IndexControllerTest {
@@ -30,7 +35,17 @@ public class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
         controller = new IndexController(recipeService);
     }
+
     @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+   @Test
     public void getIndexPage()throws Exception {
 
         //given
