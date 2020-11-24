d32a16e8b436f8ca2bda17f973248f50dad3f8dd
rtnkanth <rtnkanth@gmail.com>
2020-11-24 18:19:36 +0530

    Implemented IndexController Test

--- src/test/java/rtnk/springframework/controllers/IndexControllerTest.java
+++ src/test/java/rtnk/springframework/controllers/IndexControllerTest.java
@@ -2,10 +2,15 @@ package rtnk.springframework.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import rtnk.springframework.domain.Recipe;
 import rtnk.springframework.services.RecipeService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -27,10 +32,23 @@ public class IndexControllerTest {
     }
     @Test
     public void getIndexPage()throws Exception {
+
+        //given
+        Set<Recipe>recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        //when
         String viewName = controller.getIndexPage(model);
 
+        //then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe>setInController=argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 }
